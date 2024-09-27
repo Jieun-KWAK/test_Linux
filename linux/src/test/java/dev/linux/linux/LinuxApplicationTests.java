7a5933ae385a471d294a76140de587795f985b15
Jieun-KWAK <jieun47631@gmail.com>
2024-09-27 14:49:27 +0900

    test linux

---
+++ linux/src/test/java/dev/linux/linux/LinuxApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.linux.linux;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LinuxApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
